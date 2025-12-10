7756cf8682d6497dd962ce77edb797ddb1764bba
yihao208 <3900840748@qq.com>
2025-12-10 16:54:45 +0800

    Refactor button.cpp for button state handling

--- time/2025.12.10/button.cpp
+++ time/2025.12.10/button.cpp
@@ -51,3 +51,47 @@ void loop()
     b = 0;
   }
 }
+
+
+====================================================================================
+// C++ code
+//
+void setup()
+{
+  pinMode(7, INPUT);
+  Serial.begin(9600);
+}
+
+int last_mode = 0;
+int new_mode = 0;
+bool a;
+
+void loop()
+{
+  a = digitalRead(7);
+  //Serial.println(a);
+  if(a == 1){
+    new_mode = 1;
+    //Serial.println("1");
+  }
+  else if(a == 0){
+    new_mode = 2;
+    //Serial.println("2");
+  }
+  
+  if (new_mode != last_mode){
+    last_mode = new_mode;
+    //Serial.println(last_mode);
+    switch(new_mode){
+      case 1:
+        Serial.println("11111");
+        break;
+      case 2:
+        Serial.println("22222");
+        break;
+    
+    
+    }
+  
+  }
+}
